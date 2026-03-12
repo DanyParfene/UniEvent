6d3a31f1dac3c1183b5641d36cd8be3118f4b1a5
SchiporDevis <devis.schipor@outlook.com>
2026-03-12 21:10:39 +0200

    Partner section (card and list)

---
+++ FRONTEND/UniEvent/src/components/partners/PartnerCard.tsx
@@ -0,0 +1,61 @@
+// will be getting the name and logo as props from the PartersList
+
+import type { Partner } from "../../types/Partner";
+import editIcon from "../../assets/edit_icon.svg";
+import deleteIcon from "../../assets/delete_icon.svg";
+
+interface PartnerCardProps {
+  partner: Partner;
+  isEditMode: boolean;
+  onEdit?: () => void;
+  onDelete?: () => void
+}
+
+export const PartnerCard = ({ partner, isEditMode, onEdit, onDelete } : PartnerCardProps ) => {
+  return (
+    <div className="group relative flex flex-col items-center justify-center p-6 bg-white border border-gray-100 rounded-2xl transition-all duration-300 hover:shadow-xl hover:shadow-blue-500/5 hover:border-[#2588E7] hover:-translate-y-1">
+
+      {/* edit/delete buttons when Edit Mode is active */}
+      {isEditMode && (
+        <div className="absolute top-3 right-3 flex gap-2 z-10">
+          {/* Edit button */}
+          <button
+            onClick={onEdit}
+            className="group/btn p-1.5 bg-white border border-gray-200 rounded-md shadow-sm hover:bg-[#2588E7] hover:border-white transition-all cursor-pointer"
+            title="Edit"
+          >
+            <img 
+              src={editIcon}
+              alt="Edit"
+              className="w-4 h-4 group-hover/btn:brightness-0 group-hover/btn:invert"
+            />
+          </button>
+          
+          {/* Delete button */}
+          <button
+            onClick={onDelete}
+            className="group/delete p-1.5 bg-white border border-gray-200 rounded-md shadow-sm hover:bg-red-500 hover:border-white transition-all cursor-pointer"
+            title="Delete">
+              <img 
+              src={deleteIcon} 
+              alt="Delete" 
+              className="w-4 h-4 group-hover/delete:brightness-0 group-hover/delete:invert"
+              />
+          </button>
+        </div>
+      )}
+
+      <div className="h-16 w-full flex items-center justify-center mb-4">
+        <img 
+          src={partner.logo} 
+          alt={`${partner.name} logo`} 
+          className="max-h-full max-w-35 object-contain filter opacity-80 transition-all duration-300 group-hover:grayscale-0 group-hover:opacity-100 group-hover:scale-110" 
+        />
+      </div>
+
+      <p className="text-sm font-semibold text-gray-500 transition-colors duration-300 group-hover:text-[#033A89]">
+        {partner.name}
+      </p>
+    </div>
+  );
+};
---
+++ FRONTEND/UniEvent/src/components/partners/PartnersList.tsx
@@ -0,0 +1,62 @@
+import { PartnerCard } from "./PartnerCard";
+import { type Partner } from "../../types/Partner";
+import nokiaLogo from "../../assets/nokia_logo.png";
+import continentalLogo from "../../assets/continental_logo.png";
+import atosLogo from "../../assets/atos_logo.png";
+import bcrLogo from "../../assets/bcr_logo.png";
+import { useState } from "react";
+
+const partners: Partner[] = [
+    {id: 0, name: "Nokia", logo: nokiaLogo},
+    {id: 1, name: "Continental", logo: continentalLogo},
+    {id: 2, name: "Atos", logo: atosLogo},
+    {id: 3, name: "BCR", logo: bcrLogo},
+    
+];
+
+export const PartnersList = () => {
+    const [isEditMode, setIsEditMode] = useState(false);
+    return (
+        <section className="max-w-7xl mx-auto py-16 px-4">
+            <div className="mb-12">
+                <h2 className="text-3xl font-bold text-[#121212] mb-4 border-l-4 border-[#033A89] pl-4">
+                    Parteneriate
+                </h2>
+                <p className="text-gray-600 leading-relaxed max-w-5xl">
+                    
+                    Proiectele educaţionale, de cercetare ştiinţifică, culturale, artistice şi sportive implementate de către UVT au primit un sprijin real, activ din partea partenerilor, entităţi reprezentative din mediul socio-economic, cultural, artistic şi sportiv de la nivel regional, naţional şi internaţional.
+
+                    În ultimii ani, proiectele de impact ale UVT pe plan local, regional și național au fost susţinute de parteneri din mediul socio-economic:
+                </p>
+            </div>
+            {/* Containerul FLEX */}
+            <div className="flex flex-wrap justify-center gap-8 mb-12">
+                {partners.map((item) => (
+                    // W-full pe mobil, w-[calc(50%-1rem)] pe tableta, w-64 pe desktop
+                    <div key={item.id} className="w-full sm:w-[calc(50%-1rem)] md:w-64">
+                        <PartnerCard partner={item} isEditMode={isEditMode} onEdit={undefined} onDelete={undefined}/>
+                    </div>
+                ))}
+            </div>
+
+            {/* Admin controls area */}
+            <div>
+                <button 
+                    onClick={() => setIsEditMode(!isEditMode)}
+                    className="px-6 py-2 mx-2 bg-[#033A89] text-white rounded-lg font-medium hover:bg-[#2588E7] transition-colors shadow-sm cursor-pointer"
+                >
+                    
+                    {isEditMode ? "Ieși din Editare" : "Administrează"}
+                </button>
+
+                {isEditMode && (
+                    <button className="px-6 py-2 mx-2 bg-[#033A89] text-white rounded-lg font-medium hover:bg-[#2588E7] transition-colors shadow-sm cursor-pointer">
+                        + Adaugă Partener Nou
+                    </button>
+                )}
+            </div>
+
+            
+        </section>
+    );
+}
