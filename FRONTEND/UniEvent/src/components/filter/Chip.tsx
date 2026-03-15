3755f07e565c4825317fb7e81ef91ad9ccf71814
Maya Grozav <mayamedeea.grozav@gmail.com>
2026-03-15 21:25:02 +0200

    chip update

--- FRONTEND/UniEvent/src/components/filter/Chip.tsx
+++ FRONTEND/UniEvent/src/components/filter/Chip.tsx
@@ -7,8 +7,8 @@ type Props = {
 
 const Chip = ({ value, onClose }: Props) => {
   return (
-    <div className="flex flex-row rounded-full px-3 py-1 gap-1 shadow-sm hover:shadow-md bg-accent text-text-secondary ">
-      <CloseImg onClick={onClose} className="h-6 w-6 cursor-pointer"/>
+    <div className="flex flex-row rounded-full px-3 py-1 gap-1 shadow-sm hover:shadow-md bg-accent text-text-secondary items-center">
+      <CloseImg onClick={onClose} className="h-5 w-5 cursor-pointer"/>
       {value}
     </div>
   );
