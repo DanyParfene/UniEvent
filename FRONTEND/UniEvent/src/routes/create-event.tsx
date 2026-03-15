21366a00e8de80738265e3fcaa0371b48fff4e12
Parfene Daniel <160225733+ParfeneDaniel@users.noreply.github.com>
2026-03-15 21:42:49 +0200

    add form functionalities

--- FRONTEND/UniEvent/src/routes/create-event.tsx
+++ FRONTEND/UniEvent/src/routes/create-event.tsx
@@ -131,7 +131,6 @@ function RouteComponent() {
               setCurrentStep((prev) => prev + 1);
               setFormErrors(null);
             }}
-            className="px-6 py-2 w-fit rounded-md bg-secondary text-text-primary text-lg hover:shadow-lg hover:bg-primary hover:cursor-pointer transition-all duration-300"
           >
             Continue
           </button>
