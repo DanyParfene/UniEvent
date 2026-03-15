f2e1c9515c805ecd05798eae968c23a49bfb0ae0
Maya Grozav <mayamedeea.grozav@gmail.com>
2026-03-15 21:19:25 +0200

    chip design

--- FRONTEND/UniEvent/src/components/filter/Chip.tsx
+++ FRONTEND/UniEvent/src/components/filter/Chip.tsx
@@ -1,15 +1,14 @@
-import CloseImg from "../../assets/close.svg";
-import SmallImg from "../common/SmallImg";
+import CloseImg from "../../assets/close.svg?react";
 
 type Props = {
-  value: string;
+  value: string ;
   onClose: () => void;
 };
 
 const Chip = ({ value, onClose }: Props) => {
   return (
-    <div>
-      <SmallImg image={CloseImg} onClick={onClose}/>
+    <div className="flex flex-row rounded-full px-3 py-1 gap-1 shadow-sm hover:shadow-md bg-accent text-text-secondary ">
+      <CloseImg onClick={onClose} className="h-6 w-6 cursor-pointer"/>
       {value}
     </div>
   );
