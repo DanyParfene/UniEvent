16f89efa801552c9bdd0ea9cf9b311248184a00a
RaulSzoboszlai <raulszoboszlai@gmail.com>
2026-05-19 18:57:27 +0300

    small changes

--- FRONTEND/UniEvent/src/routes/filtrare-evenimente.tsx
+++ FRONTEND/UniEvent/src/routes/filtrare-evenimente.tsx
@@ -6,9 +6,13 @@ export const Route = createFileRoute('/filtrare-evenimente')({
 })
 
 function RouteComponent() {
+  const handleSearch = () => {
+
+  }
+
   return (
-    <div className='w-full flex flex-col items-center justify-center bg-gray-50 py-12 px-4 min-h-[calc(100vh-5rem)]'>
-      <FilterCard title="Filtrare evenimente"/>
+    <div className='w-full flex flex-col items-center justify-center py-12 px-4 min-h-[calc(100vh-5rem)]'>
+      <FilterCard title="Filtrare evenimente" onSearch={handleSearch}/>
     </div>
   )
 }
